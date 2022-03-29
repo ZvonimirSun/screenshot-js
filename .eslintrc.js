b7048024563c2ca9c8a13be6a34013070c734854
ZvonimirSun <hi@iszy.xyz>
2022-03-29 21:14:56 +0800

    style: eslint fix

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['eslint:recommended', 'standard'],
+  parser: '@babel/eslint-parser',
+  rules: {
+    'no-console': 0,
+    'no-debugger': 0,
+    'no-empty': 0,
+    'no-template-curly-in-string': 0,
+    'no-eval': 0,
+    'no-useless-escape': 0,
+    'no-loss-of-precision': 0,
+    'no-unused-vars': 0
+  }
+}
